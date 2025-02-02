8f3b6c206d0def7201c097505e1b5f0326cd9946
Arief-AK <mariefak22@gmail.com>
2025-02-02 19:16:17 +0100

    code cleanup

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 from tensorflow import keras
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 
