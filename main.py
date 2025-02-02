396bc24078396e3f166628f145544141e32f87a1
Arief-AK <mariefak22@gmail.com>
2025-02-02 19:05:52 +0100

    added GPU compatibility using TensorFlow

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 
 IMAGE_DIRECTORY = 'images/'
+USE_TENSORFLOW = True
 
 # Preprocess the data
 def preprocess_data(x_train: np.ndarray, x_test: np.ndarray) -> tuple:
@@ -42,6 +45,25 @@ def display_sample_images(x_train: np.ndarray):
         axes[i].axis('off')
     plt.savefig(f"{IMAGE_DIRECTORY}sample_images.png")
 
+# Create logistic regression model
+def create_logistic_regression_model(x_train: np.ndarray, y_train: np.ndarray) -> LogisticRegression:
+    model = LogisticRegression(max_iter=1000, solver='saga', multi_class='multinomial')
+    model.fit(x_train, y_train)
+
+    return model
+
+# Create logistic regression model using TensorFlow
+def tensor_create_logistic_regression_model(x_train: np.ndarray, y_train: np.ndarray) -> tf.keras.Model:
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(784,)),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
 if __name__ == "__main__":
     # Load the dataset
     (x_train, y_train), (x_test, y_test) = load_data()
@@ -49,13 +71,18 @@ if __name__ == "__main__":
     # Display some sample images
     display_sample_images(x_train)
 
-    # Train a logistic regression model
-    model = LogisticRegression(max_iter=1000, solver='saga', multi_class='multinomial')
-    model.fit(x_train, y_train)
+    # Ensure GPU is available
+    print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
 
-    # Predict on the test data
-    y_pred = model.predict(x_test)
+    if(USE_TENSORFLOW == False):
+        # Get logistic regression model using scikit-learn
+        logistic_regression_model = create_logistic_regression_model(x_train, y_train)
+        y_pred = logistic_regression_model.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"Logistic Regression Accuracy: {accuracy * 100:.2f}%")
 
-    # Evaluate the model
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Logistic Regression Accuracy: {accuracy * 100:.2f}%")
+    # Get logistic regression model using TensorFlow
+    tensor_model = tensor_create_logistic_regression_model(x_train, y_train)
+    tensor_model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+    test_loss, test_acc = tensor_model.evaluate(x_test, y_test)
+    print(f"TensorFlow Model Accuracy: {test_acc * 100:.2f}%")
